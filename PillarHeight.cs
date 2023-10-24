53eda3bfafc85365bffe11db95253aa1aa7810cb
mashalll <59894881+mashalll@users.noreply.github.com>
2023-10-24 16:00:00 -0400

    Add files via upload

---
+++ PillarHeight.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class PillarHeight : MonoBehaviour
+{
+    public float maxHeight = 10f; // Set the maximum height for each cube in the Inspector
+}
